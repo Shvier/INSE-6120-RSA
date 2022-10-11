c916ea0997d4552182d099aa051cf04996dc55bc
Shvier <Shvier1894@gmail.com>
2022-10-11 11:52:22 -0400

    added encrypt function

--- rsa.py
+++ rsa.py
@@ -1,6 +1,5 @@
 from cmath import sqrt
-from operator import mod, truediv
-from random import randint, random
+from random import randint
 
 def gcd(m, n):
     r = n
@@ -44,6 +43,36 @@ def findMultiInverse(base, modulo):
         x1 = x
     return (x1 if x1 > 0 else x1 + modulo)
 
+def convertDecimalToBinary(n):
+    binaries = []
+    while n != 0:
+        remainder = n % 2
+        binaries.append(remainder)
+        n = n // 2
+    binaries.reverse()
+    return binaries
+
+def mod(base, exponent, modulo):
+    binaries = convertDecimalToBinary(exponent)
+    length = len(binaries)
+    remainders = []
+    remainders.append(base % modulo)
+    for i in range(1, length):
+        r = pow(remainders[i - 1], 2) % modulo
+        remainders.append(r)
+    result = 1
+    for i in range(0, length):
+        bit = binaries[length - 1 - i]
+        if bit == 0:
+            continue
+        result = result * remainders[i] % modulo
+    return result
+
+def toDecimal(str):
+    s = str.encode('utf-8')
+    n = int(s.hex(), 16)
+    return n
+
 class RSAResolver:
     N = 0
     e = 0
@@ -75,5 +104,15 @@ class RSAResolver:
         self.e = e
         self.d = d
         print(p, q, N, e, d)
+    
+    def encrypt(self, msg, chunkSize = 3):
+        splittedMsg = [msg[i:i+chunkSize] for i in range(0, len(msg), chunkSize)]
+        decimalMsg = list(map(toDecimal, splittedMsg))
+        result = []
+        for m in decimalMsg:
+            cipher = mod(m, self.e, self.N)
+            result.append(cipher)
+        return result
 
 rsa = RSAResolver()
+rsa.encrypt("Hello World")
